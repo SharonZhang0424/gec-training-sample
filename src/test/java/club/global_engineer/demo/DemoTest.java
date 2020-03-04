155b2ffcdead6f48fa528b52de80bb1e76744c99
git4hkr <4web@mokumet.com>
2020-03-05 00:04:48 +0900

    work

--- src/test/java/club/global_engineer/demo/DemoTest.java
+++ src/test/java/club/global_engineer/demo/DemoTest.java
@@ -9,7 +9,26 @@ class DemoTest {
 		int a = 10;
 		int b = a++ + a + a-- - a-- + ++a;
 		System.out.println(b);
-
 	}
 
+	@Test
+	void 文字列操作() {
+		String str = null;
+
+		if (str == null) {
+			str = "あああああ";
+		}
+		System.out.println(str);
+
+		//splitで分割
+		// concat、joinで連結
+		//substringで範囲を切り出す
+		//containsで有無の確認
+		//indexOfで位置の検索
+		//startsWith、endsWithで始まりと終わりを検索
+		//formatで書式付き文字列を出力
+		//toUpperCase、toLowerCaseで大文字と小文字を変換する
+		//matchesでパターンのマッチング
+		//replaceとreplaceAllで置換
+	}
 }
